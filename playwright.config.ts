4193b9f264d3678b2ba3f7c4746c5ead9665492b
Mary Felvie <mary.feliciano@gmail.com>
2025-08-01 21:14:20 -0300

    add conditional statement and playwright report to be uploaded

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,31 +3,19 @@
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
-    testDir: './tests',
-    /* Maximum time one test can run for. */
-    timeout: 30 * 1000,
-    expect: {
-      /**
-       * Maximum time expect() should wait for the condition to be met.
-       * For example in `await expect(locator).toHaveText();`
-       */
-      timeout: 5000,
-    },
-    /* Run tests in files in parallel */
-    fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-    use: {
-      baseURL: `http://127.0.0.1:${process.env.PORT}`,
-    },
-  };
-  
-  export default config;
-  
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  use: {
+    baseURL: `http://127.0.0.1:${process.env.PORT}`,
+  },
+};
+
+export default config;
